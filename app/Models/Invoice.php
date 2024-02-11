d691c0c5e87d19fad5859c94f0c63a6d1bb6b975
gin-muhlis <ginxrpl07@gmail.com>
2024-02-11 20:53:21 +0700

    add: invoice api untuk menghandle data tagihan gabungan

---
+++ app/Models/Invoice.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model {
+	use HasFactory;
+
+	protected $fillable = ['uuid', 'member_id', 'principal_saving', 'mandatory_saving', 'speial_mandatory_saving', 'voluntary_saving', 'recretional_saving', 'receivable', 'account_receivable', 'month_year', 'user_id'];
+
+	public function member() {
+		return $this->belongsTo(Member::class);
+	}
+
+	public function user() {
+		return $this->belongsTo(User::class);
+	}
+}
---
+++ database/migrations/2024_02_11_101435_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('invoices', function (Blueprint $table) {
+			$table->id();
+			$table->uuid('uuid')->unique();
+			$table->unsignedBigInteger('member_id');
+			$table->unsignedBigInteger('user_id');
+			$table->double('principal_saving');
+			$table->double('mandatory_savong');
+			$table->double('special_mandatory_saving');
+			$table->double('voluntary_saving');
+			$table->double('recretional_saving');
+			$table->double('receivable');
+			$table->double('account_receivable');
+			$table->string('month_uear', 20);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('invoices');
+	}
+};
